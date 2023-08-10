4a15bbd3776a18898af867483159f1a5aa624c10
Alfred <shengyong.huang@innospot.online>
2023-08-10 14:41:34 +0800

    fix: oauth2-auth-api

--- innospots-connector-schema/src/main/java/io/innospots/connector/schema/controller/CredentialOauth2Controller.java
+++ innospots-connector-schema/src/main/java/io/innospots/connector/schema/controller/CredentialOauth2Controller.java
@@ -19,33 +19,23 @@
 package io.innospots.connector.schema.controller;
 
 import io.innospots.base.constant.PathConstant;
-import io.innospots.base.data.minder.DataConnectionMinderManager;
-import io.innospots.base.data.schema.AppCredentialInfo;
-import io.innospots.base.data.schema.ConnectionCredential;
-import io.innospots.base.data.schema.reader.IConnectionCredentialReader;
-import io.innospots.base.exception.ValidatorException;
-import io.innospots.base.json.JSONUtils;
-import io.innospots.base.model.response.InnospotResponse;
-import io.innospots.base.store.CacheStoreManager;
 import io.innospots.connector.schema.service.Oauth2CallbackService;
-import io.innospots.libra.base.operator.SystemTempCacheOperator;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.MapUtils;
-import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.io.IOException;
 
 /**
  * @author Alfred
  * @date 2023/3/6
  */
 @Slf4j
-@RestController
-@RequestMapping(PathConstant.ROOT_PATH+"oauth2")
+@Controller
+@RequestMapping(PathConstant.ROOT_PATH + "oauth2")
 @Tag(name = "App Credential Oauth2")
 public class CredentialOauth2Controller {
 
@@ -61,20 +51,19 @@ public class CredentialOauth2Controller {
             @Parameter(name = "appCode") @PathVariable String appCode,
             @Parameter(name = "code") @RequestParam("code") String code,
             @Parameter(name = "state") @RequestParam("state") String state) {
-        boolean success = authorizationCallbackService.authCallback(appCode,code, state);
-
-        return "forward:/#/metadata/oauth-credential?state="+state + "&appCode="+appCode +"&success="+success;
+        boolean success = authorizationCallbackService.authCallback(appCode, code, state);
+        return "forward:/#/oauth-result?state=" + state + "&appCode=" + appCode + "&success=" + success;
     }
 
-    @Deprecated
-    @GetMapping("status/{clientId}/{clientSecret}")
-    @Operation(summary = "oauth2 credential status")
-    public InnospotResponse<Boolean> result(
-            @Parameter(name = "clientId") @PathVariable("clientId") String clientId,
-            @Parameter(name = "clientSecret") @PathVariable("clientSecret") String clientSecret
-    ) {
-        return null;
-        //String value = systemTempCacheOperator.get(clientId + "_" + clientSecret);
-        //return InnospotResponse.success(!StringUtils.isBlank(value));
-    }
+//    @Deprecated
+//    @GetMapping("status/{clientId}/{clientSecret}")
+//    @Operation(summary = "oauth2 credential status")
+//    public InnospotResponse<Boolean> result(
+//            @Parameter(name = "clientId") @PathVariable("clientId") String clientId,
+//            @Parameter(name = "clientSecret") @PathVariable("clientSecret") String clientSecret
+//    ) {
+//        return null;
+//        //String value = systemTempCacheOperator.get(clientId + "_" + clientSecret);
+//        //return InnospotResponse.success(!StringUtils.isBlank(value));
+//    }
 }
